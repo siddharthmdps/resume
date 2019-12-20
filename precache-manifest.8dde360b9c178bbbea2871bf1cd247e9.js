7d63b40037c813280935b6c3c964253150282c2d
Siddharth SIngh <siddharthmdps@gmail.com>
2019-12-21 04:41:19 +0800

    Updates

---
+++ precache-manifest.8dde360b9c178bbbea2871bf1cd247e9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e4f5d439789c615f761fd8679d36f71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71e1f27aaa533fc4420b",
+    "url": "/static/css/2.a641e29a.chunk.css"
+  },
+  {
+    "revision": "0f5a1eeb3560c9f8095b",
+    "url": "/static/css/main.a0a21f69.chunk.css"
+  },
+  {
+    "revision": "71e1f27aaa533fc4420b",
+    "url": "/static/js/2.647ad86a.chunk.js"
+  },
+  {
+    "revision": "e6edf5f5109ad7460f75a9f5716d868f",
+    "url": "/static/js/2.647ad86a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0f5a1eeb3560c9f8095b",
+    "url": "/static/js/main.185e3974.chunk.js"
+  },
+  {
+    "revision": "3adf6062801dd9924057",
+    "url": "/static/js/runtime-main.e9aa8685.js"
+  },
+  {
+    "revision": "ed6cb1717215a04a6ef7b9faea0ac14f",
+    "url": "/static/media/Background.ed6cb171.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/resume/precache-manifest.ff2aa87ed0e9844af20fb926b1eae6a5.js"
+  "/precache-manifest.8dde360b9c178bbbea2871bf1cd247e9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/resume/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
